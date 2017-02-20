2ca1e0f07b2319c3ade2fdf94217bb41396d92a6
Hidekazu Kobayashi <hidekazu-kobayashi@cookpad.com>
2017-02-20 13:23:20 +0900

    Create indices manually & rename sequences

--- migrate-merge-streams.sql
+++ migrate-merge-streams.sql
@@ -25,7 +25,16 @@ from strload_streams
 begin;
     alter table strload_streams rename to strload_streams_old;
     alter table strload_streams_new rename to strload_streams;
+
+    alter index strload_streams_pkey rename to strload_streams_old_pkey;
     alter index strload_streams_stream_name_idx rename to strload_streams_old_stream_name_idx;
     alter index strload_streams_table_id_idx rename to strload_streams_old_table_id_idx;
-    select setval('strload_streams_new_stream_id_seq', (select last_value from strload_streams_stream_id_seq));
+
+    alter index strload_streams_new_pkey rename to strload_streams_pkey;
+    create index strload_streams_table_id_idx on strload_streams using btree (table_id);
+    create unique index strload_streams_stream_name_idx on strload_streams using btree (stream_name);
+
+    alter table strload_streams_stream_id_seq rename to strload_streams_old_stream_id_seq;
+    alter table strload_streams_new_stream_id_seq rename to strload_streams_stream_id_seq;
+    select setval('strload_streams_stream_id_seq', (select last_value from strload_streams_old_stream_id_seq));
 commit;
